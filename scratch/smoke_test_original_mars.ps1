2ee174c43d24caa545e38848cfd6ef4c27043da7
Ayush Bhardwaj <ayushchandra860@gmail.com>
2026-09-10 20:07:47 +0530

    chore: remove obsolete scratch smoke test

--- scratch/smoke_test_original_mars.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$exe = 'C:\Users\ayush\Documents\MARS\release\MARS PRO V3 3.0.0.exe'
-$process = Start-Process -FilePath $exe -PassThru
-Start-Sleep -Seconds 8
-$running = Get-Process -Id $process.Id -ErrorAction SilentlyContinue
-if (-not $running) { throw 'The packaged MARS executable exited before the smoke-test interval elapsed.' }
-$running | Select-Object ProcessName,Id,StartTime
